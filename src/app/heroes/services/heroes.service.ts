d949d253f346f08e18fd0dd453178fec1bd1d952
jere <jere77lagos@gmail.com>
2024-08-02 17:19:32 +0200

    implement heroes search page

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -11,7 +11,7 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroes():Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`)
   }
 
@@ -21,5 +21,11 @@ export class HeroesService {
         catchError( error => of(undefined) )
       )
   }
+
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+
 
 }
